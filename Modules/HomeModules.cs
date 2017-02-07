3dad8fab26b2ed20824d47982d8547364656525f
Kory Skarbek and Sara Frandsen <kory+sara@>
2017-02-07 14:28:02 -0800

    add more variables for LetterVariable add forms to match variables added

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -20,6 +20,9 @@ namespace FriendLetter
         LetterVariables myLetterVariables = new LetterVariables();
         myLetterVariables.SetRecipient(Request.Query["recipient"]);
         myLetterVariables.SetSender(Request.Query["sender"]);
+        myLetterVariables.SetCurrentLocation(Request.Query["current-location"]);
+        myLetterVariables.SetDesiredLocation(Request.Query["desired-location"]);
+        myLetterVariables.SetSouvenir(Request.Query["souvenir"]);
         return View["hello.cshtml", myLetterVariables];
       };
     }
--- Objects/LetterVariables.cs
+++ Objects/LetterVariables.cs
@@ -4,6 +4,9 @@ namespace FriendLetter.Objects
   {
     private string _recipient;
     private string _sender;
+    private string _currentLocation;
+    private string _desiredLocation;
+    private string _souvenir;
 
     public string GetRecipient()
     {
@@ -23,6 +26,36 @@ namespace FriendLetter.Objects
     public void SetSender(string newSender)
     {
       _sender = newSender;
+    }
+
+    public string GetCurrentLocation()
+    {
+      return _currentLocation;
+    }
+
+    public void SetCurrentLocation(string newCurrentLocation)
+    {
+      _currentLocation = newCurrentLocation;
+    }
+
+    public string GetDesiredLocation()
+    {
+      return _desiredLocation;
+    }
+
+    public void SetDesiredLocation(string newDesiredLocation)
+    {
+      _desiredLocation = newDesiredLocation;
+    }
+
+    public string GetSouvenir()
+    {
+      return _souvenir;
+    }
+
+    public void SetSouvenir(string newSouvenir)
+    {
+      _souvenir = newSouvenir;
     }
   }
 }
