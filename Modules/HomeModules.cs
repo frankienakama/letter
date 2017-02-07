8c9b51edcd5c1be90d14f722cdafc3ce3572a986
Kory Skarbek and Sara Frandsen <kory+sara@>
2017-02-07 13:50:26 -0800

    create letter template with forms using nancy

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,27 @@
+using Nancy;
+using FriendLetter.Objects;
+
+namespace FriendLetter
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        LetterVariables myLetterVariables = new LetterVariables();
+        myLetterVariables.SetRecipient("Eric");
+        myLetterVariables.SetSender("John");
+        return View["hello.cshtml", myLetterVariables];
+      };
+      Get["/form"] = _ => {
+        return View["form.cshtml"];
+      };
+      Get["/greeting_card"] = _ => {
+        LetterVariables myLetterVariables = new LetterVariables();
+        myLetterVariables.SetRecipient(Request.Query["recipient"]);
+        myLetterVariables.SetSender(Request.Query["sender"]);
+        return View["hello.cshtml", myLetterVariables];
+      };
+    }
+  }
+}
